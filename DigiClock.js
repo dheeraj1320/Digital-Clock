d3bebb69d0fa1d9ca31a0d5124b6ccfe59f483b2
dheeraj1320 <dheerajmanwani2000@gmail.com>
2022-08-01 23:47:06 +0530

    first commit

---
+++ DigiClock.js
@@ -0,0 +1,10 @@
+function timeabcd() {
+  let a = new Date();
+  hour = String(a.getHours()).padStart(2, 0);
+  min = String(a.getMinutes()).padStart(2, 0);
+  second = String(a.getSeconds()).padStart(2, 0);
+
+  document.querySelector(".time").textContent =
+    hour + " : " + min + " : " + second;
+}
+setInterval(timeabcd, 1000);
