bd2861a1706e876109ded09a26f26167a081ed18
dheeraj1320 <dheerajmanwani2000@gmail.com>
2022-08-04 23:53:32 +0530

    date fixed

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 // Elements
-const time_container = document.querySelector(".time");
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-function timeabcd() {
+function updateTime() {
   let date = new Date();
-  hour = String(date.getHours()).padStart(2, 0);
-  min = String(date.getMinutes()).padStart(2, 0);
-  second = String(date.getSeconds()).padStart(2, 0);
+  let hour = String(date.getHours()).padStart(2, 0);
+  let min = String(date.getMinutes()).padStart(2, 0);
+  let second = String(date.getSeconds()).padStart(2, 0);
 
-  time_container.textContent = hour + " : " + min + " : " + second;
+  document.querySelector(".time").textContent = `${hour}  : ${min} : ${second}`;
 }
-setInterval(timeabcd, 1000);
+setInterval(updateTime, 1000);
+
+function updateDate() {
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth();
+  let year = date.getFullYear();
+  document.querySelector(".date").textContent = `${day}  - ${
+    months[month - 1]
+  } - ${year}`;
+  console.log(day, month, year);
+}
+setInterval(updateDate, 1000);
 
 const settings = function () {
-  console.log("shchh");
   document.querySelector(".rotate").classList.toggle("down");
 };
