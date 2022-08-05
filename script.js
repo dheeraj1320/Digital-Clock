787e2391ca058b6d2a1249a716b85d73c7099d14
dheeraj1320 <dheerajmanwani2000@gmail.com>
2022-08-05 23:55:33 +0530

    blink function and format time

--- script.js
+++ script.js
@@ -13,28 +13,52 @@ const months = [
   "December",
 ];
 
-function updateTime() {
+const day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+let format24hr = true;
+
+// utility functions
+updateTime = function () {
   let date = new Date();
   let hour = String(date.getHours()).padStart(2, 0);
   let min = String(date.getMinutes()).padStart(2, 0);
   let second = String(date.getSeconds()).padStart(2, 0);
 
-  document.querySelector(".time").textContent = `${hour}  : ${min} : ${second}`;
-}
+  // document.querySelector(".time").textContent = `${hour}  : ${min} : ${second}`;
+
+  if (!format24hr) hour = hour % 12;
+
+  document.querySelector(".hour").textContent = hour;
+  document.querySelector(".min").textContent = min;
+  document.querySelector(".sec").textContent = second;
+  blink();
+};
+
+blink = function () {
+  document
+    .querySelectorAll(".dots")
+    .forEach((a) => a.classList.toggle("hidden"));
+};
 setInterval(updateTime, 1000);
 
-function updateDate() {
+updateDate = function () {
   let date = new Date();
-  let day = date.getDate();
+  let dateNo = date.getDate();
+  let dayNo = date.getDay();
   let month = date.getMonth();
   let year = date.getFullYear();
-  document.querySelector(".date").textContent = `${day}  - ${
+  document.querySelector(".date").textContent = `${day[dayNo]}, ${dateNo} ${
     months[month - 1]
-  } - ${year}`;
-  console.log(day, month, year);
-}
+  }, ${year}`;
+};
 setInterval(updateDate, 1000);
 
 const settings = function () {
   document.querySelector(".rotate").classList.toggle("down");
+  document.querySelector(".setting-drawer").classList.toggle("show");
+};
+
+const changeFormat = function () {
+  format24hr = !format24hr;
+  settings();
 };
